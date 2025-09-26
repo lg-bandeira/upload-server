2cecd5df54e534a02793468a9cb5a2f783584339
Luiz Bandeira <bandeiralg13@gmail.com>
2025-09-26 17:44:04 -0300

    fix typo in upload-images function

--- src/app/functions/upload-image.ts
+++ src/app/functions/upload-image.ts
@@ -6,20 +6,20 @@ import { Readable } from 'node:stream'
 import { z } from 'zod'
 import { InvalidFileFormat } from './errors/invalid-file-format'
 
-const uploadoImageInput = z.object({
+const uploadImageInput = z.object({
   fileName: z.string(),
   contentType: z.string(),
   contentStream: z.instanceof(Readable),
 })
 
-type uploadImageInput = z.input<typeof uploadoImageInput>
+type UploadImageInput = z.input<typeof uploadImageInput>
 
 const allowedMimeTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/webp']
 
 export async function uploadImage(
-  input: uploadImageInput
+  input: UploadImageInput
 ): Promise<Either<InvalidFileFormat, { url: string }>> {
-  const { fileName, contentType, contentStream } = uploadoImageInput.parse(input)
+  const { fileName, contentType, contentStream } = uploadImageInput.parse(input)
 
   if (!allowedMimeTypes.includes(contentType)) {
     return makeLeft(new InvalidFileFormat())
