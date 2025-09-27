3a1a116dc80305851918ac9a45b346e1aa64faf1
Luiz Bandeira <bandeiralg13@gmail.com>
2025-09-27 01:11:41 -0300

    organize postgres credentials

--- src/env.ts
+++ src/env.ts
@@ -4,11 +4,6 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   PORT: z.coerce.number().default(3333),
 
-  POSTGRES_HOST: z.string(),
-  POSTGRES_PORT: z.coerce.number().default(5432),
-  POSTGRES_USER: z.string(),
-  POSTGRES_PASSWORD: z.string(),
-  POSTGRES_DB: z.string(),
   DATABASE_URL: z.url().startsWith('postgresql://'),
 
   CLOUDFLARE_ACCOUNT_ID: z.string(),
