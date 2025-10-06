959f6e05ce05974152f5a1927df269041efc7bf3
Luiz Bandeira <bandeiralg13@gmail.com>
2025-10-06 03:27:05 -0300

    fix upload image route response

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -12,7 +12,7 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
         tags: ['uploads'],
         consumes: ['multipart/form-data'],
         response: {
-          201: z.null().describe('Image uploaded successfully.'),
+          201: z.object({ url: z.string() }).describe('Image uploaded successfully.'),
           400: z.object({ message: z.string() }),
         },
       },
@@ -20,7 +20,7 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
     async (request, reply) => {
       const uploadedFile = await request.file({
         limits: {
-          fileSize: 1024 * 1024 * 2, //2 Mb
+          fileSize: 1024 * 1024 * 4, //4 Mb
         },
       })
 
@@ -39,8 +39,8 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
       }
 
       if (isRight(result)) {
-        console.log(unwrapEither(result))
-        return reply.status(201).send()
+        const { url } = unwrapEither(result)
+        return reply.status(201).send({ url })
       }
 
       const error = unwrapEither(result)
